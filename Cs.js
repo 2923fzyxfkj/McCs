d99c21810ecc96e84ce899f5ab1314c6748a884e
kzyqq00-Player <1561737625@qq.com>
2024-12-27 02:36:29 +0800

    router

---
+++ Cs.js
@@ -0,0 +1,8 @@
+import { createRouter } from "./index.js";
+
+createRouter([
+    'setblock',
+    'fill',
+    'gamemode',
+    'give'
+]);
---
+++ index.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string} command
+ */
+export function commandResultProcesser(command) {
+    alert(`命令: ${command}\n你可以直接粘贴(Ctrl+V)命令到MC中执行`);
+    navigator.clipboard.writeText(command);
+}
+
+/**
+ * @param { (string | [string, ((location: Location) => void)])[] } hashesOrHashsAndHandlers
+ */
+export function createRouter(hashesOrHashsAndHandlers) {
+    const HTMLMIMEType = 'text/html';
+    const hashes = hashesOrHashsAndHandlers.filter(item => typeof item === 'string').concat(hashesOrHashsAndHandlers.filter(item => Array.isArray(item)).map(item => item[0]));
+    window.addEventListener('hashchange', () => {
+        const hash = location.hash.slice(2);
+        if (hashes.includes(hash)) {
+            fetch('/' + hash + '.html', {
+                headers: {
+                    accept: HTMLMIMEType
+                }
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        console.groupCollapsed('网络报错信息');
+                        console.log('状态:', response.statusText);
+                        console.log('请求URL:', response.url);
+                        console.groupEnd();
+                        alert('网络错误! 请重试\n可在控制台查看详情');
+                    }
+
+                    return response.text();
+                })
+                .then(text => document.documentElement.innerHTML = new DOMParser().parseFromString(text, HTMLMIMEType).documentElement.innerHTML);
+        }
+
+        const indexOfHashOfHashesOrHashsAndHandlers = hashes.indexOf(hash);
+        if (Array.isArray(indexOfHashOfHashesOrHashsAndHandlers)) {
+            const handler = indexOfHashOfHashesOrHashsAndHandlers[1];
+            if (typeof handler === 'function') {
+                handler(location);
+            }
+        }
+    });
+}
