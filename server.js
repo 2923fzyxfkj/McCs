b797104f1c1028746cffaec297fab0ea00e03392
kzyqq00-Player <1561737625@qq.com>
2024-12-28 18:33:48 +0800

    第一版？

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+const { readFile } = require('fs/promises');
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+    console.log(req.url);
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.end(
+        await readFile(req.url === '/' ? 'main.html' : req.url.slice(1))
+            .catch((e) => { if (e.errno = -4058) { res.statusCode = 404 } })
+    );
 });
 
 server.listen(port, hostname, () => {
