d7182b30af420541ce9f6d7e191d6a38d4748bf6
kzyqq00-Player <1561737625@qq.com>
2024-12-29 03:12:18 +0800

    fix

--- pages.script/fill.js
+++ pages.script/fill.js
@@ -7,21 +7,18 @@ export default function summon() {
     var end = document.getElementsByTagName("input")[2].value;
     // 拼接命令
     var command = "/fill " + start + " " + end + " minecraft:" + id;
-    switch (undefined) {
-        case (id == ""):
-            alert("ID不能为空")
-            console.error("Variable \"ID\" is null")
-            break
-        case (start == ""):
-            alert("起始不能为空")
-            console.error("Variable \"Start\" is null")
-            break
-        case (end == ""):
-            alert("结束不能为空")
-            console.error("Variable \"End\" is null")
-            break
-        default:
-            return
+    if (id === '' || start === '' || end === '') {
+        if (id == "") {
+            alert("ID不能为空");
+            console.error("Variable \"ID\" is null");
+        } else if (start == "") {
+            alert("起始不能为空");
+            console.error("Variable \"Start\" is null");
+        } else if (end == "") {
+            alert("结束不能为空");
+            console.error("Variable \"End\" is null");
+        }
+        return;
     }
     // 输出命令
     commandResultProcesser(command, id, start, end)
--- pages.script/tp.js
+++ pages.script/tp.js
@@ -6,17 +6,15 @@ export default function summon() {
     var player2 = document.getElementsByTagName("input")[1].value;
     // 拼接命令
     var command = "/tp " + player1 + " " + player2;
-    switch (undefined) {
-        case (player1 == ""):
-            alert("玩家1不能为空")
-            console.error("Variable \"player1\" is null")
-            break
-        case (player2 == ""):
-            alert("玩家2不能为空")
-            console.error("Variable \"player2\" is null")
-            break
-        default:
-            return
+    if (player1 === '' || player2 === '') {
+        if (player1 == "") {
+            alert("玩家1不能为空");
+            console.error("Variable \"player1\" is null");
+        } else if (player2 == "") {
+            alert("玩家2不能为空");
+            console.error("Variable \"player2\" is null");
+        }
+        return;
     }
     // 输出命令
     commandResultProcesser(command, player1, player2);
