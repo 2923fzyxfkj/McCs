7bcdd2b4eeae87f2437863b4449dd198944b584f
kzyqq00-Player <1561737625@qq.com>
2024-12-28 19:33:17 +0800

    优化一下

---
+++ server/server.mjs
@@ -0,0 +1,58 @@
+import { readFile } from 'fs/promises';
+import mime from 'mime';
+import http from 'http';
+import chardet from 'chardet';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+/**
+ * @param {http.ServerResponse} res
+ * @param {Error & { code: any }} e
+ */
+function reply500(res, e) {
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({
+        '': 'Internal Server Throws Error',
+        message: e.message,
+        stack: e.stack,
+        code: e.code,
+    }));
+
+    console.error(e);
+}
+
+const server = http.createServer(async (req, res) => {
+    try {
+        const filepath = req.url.slice(1);
+
+        process.stdout.write(req.method + ' ' + req.url);
+        res.statusCode = 200;
+    
+        const file = await readFile(req.url === '/' ? 'main.html' : filepath)
+            .then(data => {
+                const encoding = chardet.detect(data);
+
+                res.setHeader('Content-Type', mime.getType(filepath).concat(encoding ? '; charset=' + encoding : ''));
+                res.setHeader('Cache-Control', 'no-cache');
+
+                return data;
+            })
+            .catch(e => {
+                if (e.errno = -4058) {
+                    res.statusCode = 404;
+                } else {
+                    reply500(res, e);
+                }
+            });
+        res.end(file);
+        console.log(' |', res.statusCode);
+    } catch (e) {
+        reply500(res, e);
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
